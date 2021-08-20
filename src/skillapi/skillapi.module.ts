ca9678d865253bf306a400627a828499edc1c996
indianpix098 <saket@indian.pix>
2021-08-20 15:00:52 +0530

    Imported meilisearch

--- src/skillapi/skillapi.module.ts
+++ src/skillapi/skillapi.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common"; 
 import { SkillApiController } from "./skillapi.controller";
 import { SkillApiService } from "./skillapi.service";
+import { MeiliSearchModule } from 'nestjs-meilisearch';
 
 @Module({
-    imports: [],
+    imports: [MeiliSearchModule.forRoot({
+        host: 'http://127.0.0.1:7700',
+        apiKey: 'masterKey',
+      }),],
     controllers: [SkillApiController],
     providers: [SkillApiService]
 })
--- src/skillapi/skillapi.service.ts
+++ src/skillapi/skillapi.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectMeiliSearch } from 'nestjs-meilisearch';
+import { MeiliSearch } from 'meilisearch';
 
 @Injectable()
 export class SkillApiService {
-
+  public constructor(
+    @InjectMeiliSearch() private readonly meiliSearch: MeiliSearch,
+  ) {}
     
   }
 
